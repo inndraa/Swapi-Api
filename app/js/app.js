65134d80129c21b74a428aa82e19dce627e0f6a5
inndraa <toruu42@gmail.com>
2023-02-15 20:04:13 +0200

    upd

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,12 @@ app.service("appData", function ($http) {
   this.getFilms = function () {
     return $http.get("https://swapi.dev/api/films");
   };
+  this.getCharacters = function () {
+    return $http.get("https://swapi.dev/api/people");
+  };
+  this.getSpecies = function () {
+    return $http.get("https://swapi.dev/api/species");
+  };
 });
 app.controller("appController", function ($scope, appData) {
   $scope.films = [];
@@ -13,5 +19,20 @@ app.controller("appController", function ($scope, appData) {
     });
   }
   getAllFilms();
+  $scope.characters = [];
+  function getAllCharacters() {
+    getPromise = appData.getCharacters();
+    getPromise.then(function (charactersList) {
+      $scope.characters = charactersList.data;
+    });
+  }
+  getAllCharacters();
+  $scope.species = [];
+  function getAllSpecies() {
+    getPromise = appData.getSpecies();
+    getPromise.then(function (speciesList) {
+      $scope.species = speciesList.data;
+    });
+  }
+  getAllSpecies();
 });
-    

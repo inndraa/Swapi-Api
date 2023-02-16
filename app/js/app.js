1a2e257d4d7e0493eb5fce8c4e38b8bb45c77d77
inndraa <toruu42@gmail.com>
2023-02-16 12:22:54 +0200

    +gets

--- app/js/app.js
+++ app/js/app.js
@@ -6,12 +6,23 @@ app.service("appData", function ($http) {
   this.getCharacters = function () {
     return $http.get("https://swapi.dev/api/people");
   };
-  this.getSpecies    = function () {
+  this.getSpecies = function () {
     return $http.get("https://swapi.dev/api/species");
   };
+  this.getPlanets = function () {
+    return $http.get("https://swapi.dev/api/planets");
+  };
+  this.getVehicles = function () {
+    return $http.get("https://swapi.dev/api/vehicles");
+  };
+  this.getStarships = function () {
+    return $http.get("https://swapi.dev/api/starships");
+  };
 });
+
 app.controller("appController", function ($scope, appData) {
   $scope.films = [];
+  // ---------------------------------
   function getAllFilms() {
     getPromise = appData.getFilms();
     getPromise.then(function (filmsList) {
@@ -19,6 +30,7 @@ app.controller("appController", function ($scope, appData) {
     });
   }
   getAllFilms();
+  // ---------------------------------
   $scope.characters = [];
   function getAllCharacters() {
     getPromise = appData.getCharacters();
@@ -27,6 +39,7 @@ app.controller("appController", function ($scope, appData) {
     });
   }
   getAllCharacters();
+  // ---------------------------------
   $scope.species = [];
   function getAllSpecies() {
     getPromise = appData.getSpecies();
@@ -35,4 +48,31 @@ app.controller("appController", function ($scope, appData) {
     });
   }
   getAllSpecies();
+  // ---------------------------------
+  $scope.planets = [];
+  function getAllPlanets() {
+    getPromise = appData.getPlanets();
+    getPromise.then(function (planetsList) {
+      $scope.planets = planetsList.data;
+    });
+  }
+  getAllPlanets();
+  // ---------------------------------
+  $scope.vehicles = [];
+  function getAllVehicles() {
+    getPromise = appData.getVehicles();
+    getPromise.then(function (vehiclesList) {
+      $scope.vehicles = vehiclesList.data;
+    });
+  }
+  getAllVehicles();
+  // ---------------------------------
+  $scope.starships = [];
+  function getAllStarships() {
+    getPromise = appData.getStarships();
+    getPromise.then(function (starshipsList) {
+      $scope.starships = starshipsList.data;
+    });
+  }
+  getAllStarships();
 });
